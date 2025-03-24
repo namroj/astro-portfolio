3e560fa3302f11204acebf80370fce212662f16e
Jorman Espinoza <espinoza.dev@gmail.com>
2025-03-24 12:35:32 -0300

    fix: Update RSS feed title to include blog context for clarity

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
     return rss({
-        title: 'Jorman Espinoza | Software Engineer',
+        title: '[Blog] Jorman Espinoza | Software Engineer',
         description: 'Personal blog of Jorman Espinoza, Software Engineer',
         site: context.site,
         items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
