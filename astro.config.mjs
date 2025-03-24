0d591aee730d9524c3ac11d2770766fceb7e6a8b
Jorman Espinoza <espinoza.dev@gmail.com>
2025-03-24 20:30:34 -0300

    feat: Integrate Preact into the Astro project, update configuration for JSX support, and enhance components with new imports and structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://namroj-astro-portfolio.netlify.app/',
+  site: 'https://namroj-astro-portfolio.netlify.app/',
+  integrations: [preact()],
 });
