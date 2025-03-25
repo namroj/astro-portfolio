1940e347b55766441d3bef5c2aa1a116aa4dabe3
Jorman Espinoza <espinoza.dev@gmail.com>
2025-03-24 21:29:35 -0300

    refactor: Enhance RSS feed generation by directly importing blog posts and simplifying item mapping for improved performance and clarity

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,20 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
 
 export async function GET(context) {
+    const posts = await import.meta.glob('../blog/**/*.md', { eager: true });
+
+    const items = Object.values(posts).map((post) => ({
+        title: post.frontmatter.title,
+        description: post.frontmatter.description,
+        pubDate: post.frontmatter.pubDate,
+        link: `/blog/${post.file.split('/').pop().split('.').shift()}`,
+    }));
+
     return rss({
-        title: '[Blog] Jorman Espinoza | Software Engineer',
+        title: '[Blog] Jorman Espinoza',
         description: 'Personal blog of Jorman Espinoza, Software Engineer',
         site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+        items: items,
         customData: `<language>en-us</language>`,
     });
 }
