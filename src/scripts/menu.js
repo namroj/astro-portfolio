8145b5bc1c2ef45dab91801c833546f6e13121b2
Jorman Espinoza <espinoza.dev@gmail.com>
2025-04-02 12:38:15 -0300

    refactor: Enhance Hamburger, Header, and Navigation components with improved styling and functionality for better user interaction

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,10 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('expanded');
+    hamburger.classList.toggle('expanded');
+    nav.classList.toggle('expanded');
 });
+
