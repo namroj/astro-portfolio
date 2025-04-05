f498d64a780ba2b229d44435c6c4c45b1f6f104d
Jorman Espinoza <espinoza.dev@gmail.com>
2025-04-05 15:43:10 -0300

    refactor: update styles and improve UI consistency
    Reorganized CSS for components to optimize structure and maintain consistent spacing, colors, and hover effects. Enhanced dark mode compatibility and adjusted layout for better responsiveness across the app. Updated JavaScript to handle new class changes for expanded states.

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('nav');
-const navLinks = document.querySelector('.navigation .links');
+const navLinks = document.querySelector('.navigation');
+const footer = document.querySelector('footer');
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('expanded');
     hamburger.classList.toggle('expanded');
     nav.classList.toggle('expanded');
+    footer.classList.toggle('expanded');
 });
