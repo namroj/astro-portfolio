a065ea5d8d9a9ae2c48788f251f82e04f21867be
Jorman Espinoza <espinoza.dev@gmail.com>
2025-04-16 13:35:08 -0300

    feat: add Sidebar component and extend type definitions
    - Added a new `Sidebar` component to the `Layout.astro` file.
    - Extended the `Font` type in `env.d.ts` to include additional font options.
    - Removed the `ThemeSwitcher` component from `Footer.astro`.
    - Updated styles in `Footer.astro` for better layout adjustments.

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,6 +8,8 @@ declare global {
 
 export type Theme = "light" | "dark" | "system";
 
+export type Font = "Cascadia Code" | "DankMono" | "Fira Code" | "Writter" | "JetBrains Mono";
+
 export interface BreadCrumbProp {
   url: string;
   label: string;
